ca3f8087a2111c3dc655b90ca5241d30901db982
Paul Poirel <poirelpa@gmail.com>
2026-07-03 14:04:43 +0000

    ICS file transform to Intervals.icu format

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <?php
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+$ics = file_get_contents('https://beta.trainasone.com/calendar/b6553640d8a7dde3cf340f9f1780c94da8d76f50.ics');
+$ics = str_replace("\r\n ",'',$ics);
+$ics = str_replace('*','-',$ics);
+$ics = str_replace('/km','/km Pace',$ics);
+$ics = str_replace(' bpm','bpm',$ics);
+while(preg_match('/(\d+) hours?\\\\, (\d+) minutes?/',$ics, $matches))
+{
+	$ics = str_replace($matches[0], ($matches[1] * 60 + $matches[2]) . "m", $ics);
+}
+$ics = str_replace(' minutes','m',$ics);
 
-function sayHello($name) {
-	echo "Hello $name!";
+header('Content-Type: text/calendar; charset=utf-8');
+header('Content-Disposition: attachment; filename="plan.ics"');
+
+$lines = explode("\r\n", $ics);
+$ics = '';
+foreach($lines as $line)
+{
+	$ics .= chunk_split($line, 75, "\r\n ") . "\r\n";
 }
 
-?>
 
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
-</html>
+
+echo $ics;
+
+?>
