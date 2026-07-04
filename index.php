a3def5402be55c65c4b524aad8d3cc41b2b353bc
Paul Poirel <poirelpa@gmail.com>
2026-07-04 09:24:35 +0000

    Amélioration du parsing des ics

--- index.php
+++ index.php
@@ -3,17 +3,31 @@
 $id = $_GET['id'];
 $ics = file_get_contents("https://beta.trainasone.com/calendar/$id.ics");
 $ics = str_replace("\r\n ",'',$ics);
-$ics = str_replace('*','-',$ics);
+$ics = str_replace('.\n','\n',$ics);
+$ics = preg_replace('/ \* Perform the following steps (\d+) times/','\nPerform the following steps $1x',$ics);
+$ics = str_replace('   *','- ',$ics);
+$ics = str_replace(' *','\nStep\n- ',$ics);
 $ics = str_replace('/km','/km Pace',$ics);
+$ics = str_replace(' km','km',$ics);
+$ics = preg_replace('/ \d+ bpm \((\d+) to (\d+)\)/',' $1-$2bpm',$ics);
 $ics = str_replace(' bpm','bpm',$ics);
-while(preg_match('/(\d+) hours?\\\\, (\d+) minutes?/',$ics, $matches))
-{
-	$ics = str_replace($matches[0], ($matches[1] * 60 + $matches[2]) . "m", $ics);
-}
+$ics = str_replace(' hours','h',$ics);
+$ics = str_replace(' hour','h',$ics);
 $ics = str_replace(' minutes','m',$ics);
+$ics = str_replace(' minute','m',$ics);
+$ics = str_replace(' seconds','s',$ics);
+$ics = str_replace(' second','s',$ics);
+$ics = str_replace('no faster than ','20:00-',$ics);
+$ics = preg_replace('/\(([0-9.]+km)\)/s','$1',$ics);
 
-header('Content-Type: text/calendar; charset=utf-8');
-header('Content-Disposition: attachment; filename="plan.ics"');
+
+
+if(false){ // debug
+
+echo '<pre>';
+echo str_replace('\,',',',str_replace('\n', "\r\n", $ics));
+die();
+}
 
 $lines = explode("\r\n", $ics);
 $ics = '';
@@ -21,9 +35,8 @@ foreach($lines as $line)
 {
 	$ics .= chunk_split($line, 75, "\r\n ") . "\r\n";
 }
-
-
-
+header('Content-Type: text/calendar; charset=utf-8');
+header('Content-Disposition: attachment; filename="plan.ics"');
 echo $ics;
 
 ?>
